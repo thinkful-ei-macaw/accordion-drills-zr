34f88e2284321d108c03d54d44b3008bb51c3dad
Raul Garcia <punkvomit21@gmail.com>
2020-03-04 15:42:44 -0500

    accordion drill complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 // import HelloWorld from './state-drills/HelloWorld';
-import RouletteGun from './state-drills/RouletteGun';
+// import RouletteGun from './state-drills/RouletteGun';
+import Accordion from './state-drills/Accordion';
 import './index.css';
 
-ReactDOM.render(<RouletteGun />, document.getElementById('root'));
+ReactDOM.render(<Accordion />, document.getElementById('root'));
---
+++ src/state-drills/Accordion.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+class Accordion extends Component {
+
+  static defaultProps = {
+    sections: [{
+      title: 'Section 1',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    },
+    {
+      title: 'Section 2',
+      content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+    },
+    {
+      title: 'Section 3',
+      content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+    },]
+
+  }
+
+  handleSetActiveSection(section) {
+    this.setState({ activeSection: section })
+  }
+
+
+  state = {
+    activeSection: null
+
+  };
+
+
+
+  renderItem(idX, activeSection, section) {
+    return (
+      <li className="AccordionItem">
+        <button onClick={() => this.handleSetActiveSection(idX)}>{section.title}</button>
+        {(activeSection === idX) && <p>{section.content}</p>}
+      </li>
+    );
+  }
+
+
+  render() {
+    const { activeSection } = this.state
+    const { sections } = this.props
+
+    return (
+      <ul className="Accordion">
+        {sections.map((section, idX) =>
+          this.renderItem(idX, activeSection, section)
+        )}
+      </ul>
+    )
+  }
+
+}
+
+export default Accordion
